188322517c8a686794e6f8c0d235b037c3a4ec79
kimsh-m <ksh050319@naver.com>
2024-10-08 10:24:51 +0900

    Refactor : 메서드 수정

--- achacha/src/main/java/com/achacha/controller/HeartRateRestController.java
+++ achacha/src/main/java/com/achacha/controller/HeartRateRestController.java
@@ -15,15 +15,14 @@ public class HeartRateRestController {
 	
 	// 심박수 변화에 따라 전송 받음 ( 데이터 베이스에 들어가는 세팅 해야 합니다  아직 안함 )
     @PostMapping("/heartrate") 
-    public ResponseEntity<String> receiveWatch(@RequestBody HeartRateDataVO watchData) {
+    public ResponseEntity<String> receiveHeartRate(@RequestBody HeartRateDataVO watchData) {
         System.out.println("Received Heart Rate: " + watchData.getHeartRate());
-        return ResponseEntity.ok("Data received");
+        return ResponseEntity.ok("Data received"); // 200 메세지를 반환( 성공 실패 )
     }
-    
     
     // 완료 누르면 시간 전송      -------------  이 메서드도 운전기록 생성되면 고쳐야 합니다.
     @PostMapping("/drivingtime") 
-    public ResponseEntity<String> receiveWatchEnd(@RequestBody HeartRateDataVO watchData) {
+    public ResponseEntity<String> receiveDrivingTime(@RequestBody HeartRateDataVO watchData) {
         System.out.println("Received Driving Time: " + watchData.getDrivingTime());
         return ResponseEntity.ok("Data received");
     }
